7fb88ec495b8fe73d1571b4c439312a9c8dfc0fa
bruxvr <brunaxavier@id.uff.br>
2022-05-18 11:34:53 -0300

    descrição da lógica do js

---
+++ script.js
@@ -0,0 +1,13 @@
+// tipoDeCripto -> escolhe cifra de césar ou base64
+// se cifra de cesar -> mostra numero de passos
+// se base 64 -> não mostra numero de passos
+// mostra opção codificar ou decodificar
+// se codificar -> campo <input> mostra Codificar!
+// se decodificar -> campo <input> mostra Decodificar!
+// button executa a função e mostra no output
+// reset volta para a escolha de opção entre cifra de cesar ou base64
+
+// CÓDIGO COMEÇA ABAIXO //
+
+// input do usuário
+
