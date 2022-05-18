10d2a2c0832b189118852a750bb5de583b6104d3
bruxvr <brunaxavier@id.uff.br>
2022-05-18 17:59:06 -0300

    lógica de botões botões

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// tipoDeCripto -> escolhe cifra de césar ou base64
+// tipoDeCodigo -> escolhe cifra de césar ou base64
 // se cifra de cesar -> mostra numero de passos
 // se base 64 -> não mostra numero de passos
 // mostra opção codificar ou decodificar
@@ -10,4 +10,59 @@
 // CÓDIGO COMEÇA ABAIXO //
 
 // input do usuário
+
+inputDoUsuario = function () {
+    let input = document.querySelector('#input')
+    input = input.value
+    return input
+}
+
+// retorno codificado/decodificado do input do usuário
+function outputDaMensagem(resultadoDaMensagem) {
+    let output = document.querySelector('#output')
+    output = output.value = '${resultadoDaMensagem}'
+    return output
+}
+
+// função para verificar qual tipo de código o usuário escolheu no radio button
+var tipoDeCodigo = document.querySelector('#codigo')
+
+tipoDeCodigo.addEventListener('change', function (tipoDeCodigo) {
+    var codigoEscolhido = tipoDeCodigo.target.value
+
+    if (codigoEscolhido == 'base64') {
+        incremento.style.display = 'none'
+        botaoPronto.setAttribute('onclick', base64())
+    }
+    else {
+        incremento.style.display = 'inline'
+        botaoPronto.setAttribute('onclick', caesarCipher())
+    }  
+
+        
+})
+
+// trocando escrita do botão -> codificar e decodificar
+var botaoPronto = document.querySelector('#magica')
+
+var encoding = document.querySelector('#encode')
+
+encoding.addEventListener('click', function() {
+    
+    botaoPronto.innerText = 'Codificar!'
+})
+
+var decoding = document.querySelector('#decode') 
+
+decoding.addEventListener('click', function() {
+    botaoPronto.innerText = "Decodificar!"
+    
+})
+
+
+
+//function tipoDeCodigo() {
+//    const option = document.querySelector('tipoDeCodigo')
+//    const caesar = select.options[select]}
+
 
