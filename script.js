047fcea9b3a6efd449c0feb9651da359d279d637
bruxvr <brunaxavier@id.uff.br>
2022-05-19 14:08:08 -0300

    ajustes no css

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // tipoDeCodigo -> escolhe cifra de césar ou base64
 // se cifra de cesar -> mostra numero de passos
 // se base 64 -> não mostra numero de passos
-// mostra opção codificar ou decodificar
+// troca opção codificar ou decodificar
 // se codificar -> campo <input> mostra Codificar!
 // se decodificar -> campo <input> mostra Decodificar!
 // button executa a função e mostra no output
-// reset volta para a escolha de opção entre cifra de cesar ou base64
+// reset limpa os campos de input e output
 
 // CÓDIGO COMEÇA ABAIXO //
 
